4e2d34ac7add649e2f6c8e9461c7a95e6d637299
Ignacio Manríquez <i.manriquez01@ufromail.cl>
2024-04-23 15:42:38 -0400

    Commit v0.3
    App

--- taller-vue/src/App.vue
+++ taller-vue/src/App.vue
@@ -1,17 +1,52 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <HelloWorld msg="Lista de quehaceres"/>
+    <!-- Crea una lista -->
+    <input v-model="listName" placeholder="Nombre de la lista">
+    <button @click="renameList">Crear Lista</button>
+    <ul>
+      <li v-for="(task, index) in tasks" :key="index">
+        {{ task }}
+        <button @click="removeTask(index)">Eliminar</button>
+        <button @click="modifyTask(index, 'Nombre Nuevo')">Modificar</button>
+      </li>
+    </ul>
+    <!-- Crea y agrega una nueva tarea -->
+    <input v-model="newTask" placeholder="Nuevo Quehacer">
+    <button @click="addTask">Agregar Tarea</button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      listName: '',
+      tasks: [],
+      newTask: '',
+    };
+  },
+  methods: {
+    // Renombra la tarea de la lista
+    renameList() {
+
+    },
+    // Agrega una tarea de la lista
+    addTask() {
+      this.tasks.push(this.newTask);
+      this.newTask = '';
+    },
+    // Remueve una tarea de la lista
+    removeTask(index) {
+      this.tasks.splice(index, 1);
+    },
+    // Modifica una tarea de la lista
+    modifyTask(index, newValue) {
+      this.tasks[index] = newValue;
+    },
+  },
+};
 </script>
 
 <style>
